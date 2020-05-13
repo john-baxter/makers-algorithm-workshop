e46586a472a68965c0d7211ba9f6af868e3bd448
john-baxter <john.g.baxter@googlemail.com>
2020-05-13 16:00:02 +0100

    rename Looping to TestRun, tidy up code

--- feature_test.rb
+++ feature_test.rb
@@ -1,17 +1,4 @@
-# require './timing_code.rb'
-require './looping.rb'
+require './test_run.rb'
 
-# test_array_first = 1
-# test_array_last = 5000
-
-# test_array = *test_array_first..test_array_last
-
-# attempt = TimingCode.new
-# attempt.timing_code(test_array, :shuffle)
-
-
-attempt = Looping.new
-
-# puts attempt.counter
+attempt = TestRun.new
 attempt.one_test_run(:shuffle)
-# puts counter
--- timing_code.rb
+++ timing_code.rb
@@ -1,15 +1,20 @@
 class TimingCode
 
   def timing_code (array, method_to_time)
-    start = time_now_ms
+    # start = time_now_ms
+    start = time_now_micro_s
     array.send(method_to_time)
-    fin = time_now_ms
+    # fin = time_now_ms
+    fin = time_now_micro_s
     time_calc(start, fin)
   end
 
   def time_now_ms
-    # Time.now.to_f
     (Time.now.to_f * 1000).to_i
+  end
+  
+  def time_now_micro_s
+    (Time.now.to_f * 1000000).to_i
   end
 
   def time_calc(t_one, t_two)
