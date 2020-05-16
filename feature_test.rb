f73312eb9a78c85ad9ce6b274c29836bc09a60d3
john-baxter <john.g.baxter@googlemail.com>
2020-05-16 21:04:22 +0100

    replace data in SS with new data taken using new timing code. add in data for my_reverse and update chart

--- feature_test.rb
+++ feature_test.rb
@@ -5,7 +5,7 @@ require './home_made_functions.rb'
 # puts my_reverse(array)
 
 attempt = TestLooping.new
-# # attempt.times_loop(:shuffle)
+# attempt.times_loop(:shuffle)
 # attempt.times_loop(:reverse)
 # attempt.times_loop(:sort)
 attempt.times_loop(:my_reverse)
