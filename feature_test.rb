86157541fed7f819b888dc5cadc1816257a1aec0
john-baxter <john.g.baxter@googlemail.com>
2020-05-17 09:34:10 +0100

    add my_shuffle function and corresponding data to the spreadsheet

--- feature_test.rb
+++ feature_test.rb
@@ -3,9 +3,11 @@ require './home_made_functions.rb'
 
 # array = *1..5
 # puts my_reverse(array)
+# puts my_shuffle(array)
 
 attempt = TestLooping.new
 # attempt.times_loop(:shuffle)
 # attempt.times_loop(:reverse)
 # attempt.times_loop(:sort)
-attempt.times_loop(:my_reverse)
+# attempt.times_loop(:my_reverse)
+attempt.times_loop(:my_shuffle)
--- home_made_functions.rb
+++ home_made_functions.rb
@@ -8,28 +8,31 @@ end
 
 def my_shuffle(array)
   result_array = []
+  while array.length != 0
+    result_array << array.delete_at(rand(array.length))
+    # implement some stuff here
+    # seems to be working!
+  end
+  result_array
+end
+  
+
+
 
 =begin
   take a random element
+  
+
   place it into the new array
+  
+  
   remove it from the original array
+  
+  
   repeat
 
-  OR
+  
 
-  take the first OR last element (I think it wouldn't matter which)
-  randomly shove it into the new array
-  remove it from the original array
-  repeat
 
-  OR
-
-  take a random element
-  randomly shove it into the new array
-  remove it from the original array
-  repeat
-
-  The last one seems less favourable since we need to generate a random number twice per iteration, and it's unlikely this would help create "more randomness"
-  The first two seem probably pretty similar and since .push exists and seems easier to implement than 'shoving' elements into a random spot; I'll go for the first one.
 
 =end
