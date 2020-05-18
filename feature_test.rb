94003d1d3d429c18e4b7fbf0414ac7114c57e60e
john-baxter <john.g.baxter@googlemail.com>
2020-05-18 17:37:03 +0100

    post workshop commit, no change since last commit, 'linear' shuffle is actually quadratic

--- feature_test.rb
+++ feature_test.rb
@@ -1,10 +1,12 @@
 require './looping.rb'
 require './home_made_functions.rb'
 
-attempt = TestLooping.new
-# attempt.times_loop(:shuffle)
-# attempt.times_loop(:reverse)
-# attempt.times_loop(:sort)
-# attempt.times_loop(:my_reverse)
-attempt.times_loop(:my_shuffle)
+# attempt = TestLooping.new
+# # attempt.times_loop(:shuffle)
+# # attempt.times_loop(:reverse)
+# # attempt.times_loop(:sort)
+# # attempt.times_loop(:my_reverse)
+# attempt.times_loop(:my_shuffle)
 
+array = *1..100
+puts my_shuffle(array)
