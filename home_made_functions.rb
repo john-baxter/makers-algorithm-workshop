0f12500d18b4877cc6106af0147724039112ad18
john-baxter <john.g.baxter@googlemail.com>
2020-05-18 10:32:27 +0100

    second attempt at my_shuffle

--- home_made_functions.rb
+++ home_made_functions.rb
@@ -6,12 +6,21 @@ def my_reverse(array)
   result_array
 end
 
-def my_shuffle(array)
+def my_shuffle_(array)
   result_array = []
   while array.length != 0
     result_array << array.delete_at(rand(array.length))
   end
   result_array
 end
+
+def my_shuffle(array)
+  result_array = []
+  array.each do |element|
+    result_array.insert(rand(result_array.length), element)
+  end
+  result_array
+end
+
 
 
