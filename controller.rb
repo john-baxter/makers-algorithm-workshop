48d7a15053584458bad7d47465fa0574eb5775df
john-baxter <john.g.baxter@googlemail.com>
2020-06-11 15:16:02 +0100

    add functioning my-merge-sort and data.

--- controller.rb
+++ controller.rb
@@ -24,20 +24,22 @@ attempt = TestLooping.new
 # attempt.times_loop(:my_fibonacci_sequence_generator_3, :fibonacci_start_2)
 # attempt.times_loop(:my_selection_sort, :int_array_shuffle)
 
+attempt.times_loop(:my_merge_sort, :int_array_shuffle)
 
 # attempt.times_loop(:function, :array_generator)
 # attempt_ = TestLooping.new
 # attempt_.times_loop(:my_insertion_sort, :int_array_shuffle)
 
 
-testing_array_of_two_arrays = [[1,4,7,9,10,11],[2,3,5]]
-testing_array = [1,2,3,4,5,6,7,8,9].shuffle
-two_sorted_arrays = [[2,4,6,8],[1,3,5,7]]
-big_testing_array = [*1..100].shuffle
-# p my_early_return_for_sorting(testing_array)
-# p my_split_array_left_and_right(testing_array)
-# p my_split_array_lower_higher_than_first(testing_array)
-# p my_merge_two_ordered_arays(testing_array_of_two_arrays)
-p big_testing_array
-p my_merge_sort(big_testing_array)
+# testing_array_of_two_arrays = [[1,4,7,9,10,11],[2,3,5]]
+# testing_array = [1,2,3,4,5,6,7,8,9].shuffle
+# two_sorted_arrays = [[2,4,6,8],[1,3,5,7]]
+# big_testing_array = [*1..100].shuffle
+# # p my_early_return_for_sorting(testing_array)
+# # p my_split_array_left_and_right(testing_array)
+# # p my_split_array_lower_higher_than_first(testing_array)
+# # p my_merge_two_ordered_arays(testing_array_of_two_arrays)
+# p big_testing_array
+# p my_merge_sort(big_testing_array)
 # p my_merge_two_ordered_arays(two_sorted_arrays[0], two_sorted_arrays[1])
+
